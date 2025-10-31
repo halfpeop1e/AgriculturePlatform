f53ca58d406b3e4b794d0cd6814870686fca7e3e
halfpeop1e <halfpeop1e@outlook.com>
2025-10-31 16:58:57 +0800

    修改了发布产品的POST需求数据和购买产品的API

--- composables/useProduct.ts
+++ composables/useProduct.ts
@@ -1,9 +1,16 @@
 import { useAxios } from "./useAxios";
 import type{ postProductRequest } from "~/types/product";
+import{v4 as uuidv4} from 'uuid'
+
 const useAxiosInstance=useAxios()
-export async function PostProduct(formData:postProductRequest){
+export async function PostProduct(formData:any){
+    const UserStore=useUserStore()
     try{
-        const response=await useAxiosInstance.post('/product/sell',formData,{
+        const response=await useAxiosInstance.post<postProductRequest>('/product/sell',{
+            formData,
+            salerId:UserStore.userinfo.userId
+        },
+        {
             headers:{
                 'Content-Type':'multipart/form-data'
             }
@@ -19,5 +26,28 @@ export async function PostProduct(formData:postProductRequest){
     }
     catch(err){
         console.error('发布产品失败',err)
+    }
+}
+export async function BuyProduct(productId:number,quantity:number,totalprice:number){
+    try{
+        const response=await useAxiosInstance.post('/product/buy',{
+            orderId:uuidv4(),
+            productId:productId,
+            quantity:quantity,
+            totalprice:totalprice
+            // buyer:useUserStore().userinfo.userId,
+            // saler:从商品的发布者信息中获取
+        })
+        if(response.status===200){
+            console.log('购买产品成功',response.data)
+            ElMessage.success('购买产品成功')
+            return response
+        }
+        else{
+            throw new Error('购买产品失败')
+        }
+    }
+    catch(err){
+        console.error('购买产品失败',err)
     }
 }   
