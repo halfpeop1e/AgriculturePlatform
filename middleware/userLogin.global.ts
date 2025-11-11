ecb442e7e4679f4124ccae213c63ab1a631383fb
halfpeop1e <halfpeop1e@outlook.com>
2025-11-11 19:50:28 +0800

    修改最后活跃时间逻辑

--- middleware/userLogin.global.ts
+++ middleware/userLogin.global.ts
@@ -1,13 +1,17 @@
 export default defineNuxtRouteMiddleware((to) => {
     // 允许访问的公开路径
-    const publicPaths = ['/','/homepage', '/login', '/register']
+    const publicPaths = ['/','/homePage', '/login', '/register']
 
     // 如果是公开路径，直接放行
-    if (publicPaths.includes(to.path)) return
-
+    if (publicPaths.includes(to.path)) {
+      return
+    }
     // 检查是否已登录（通过 cookie 中的 AuthToken）
     const authToken = useCookie('AuthToken')?.value
 
     // 如果没有登录凭证，重定向到登录页
-    if (!authToken) return navigateTo('/login')
+    if (!authToken) {
+      ElMessage.error('请先登录')
+      return navigateTo('/login')
+    }
 })
--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="flex items-center justify-center h-screen">
-        <LoginForm class="mb-20" @register="handleRegister"/>
+        <LoginForm class="mb-20" @register="handleRegister" @setactivetime="SetActiveTime"/>
     </div>
 </template>
 
 <script setup lang="ts">
 import LoginForm from '~/components/loginForm.vue';
-
+import moment from 'moment';
 definePageMeta({ layout: 'home-page-layout' })
 
 const handleRegister=()=>{
     navigateTo('/register')
 }
+function SetActiveTime(){
+    const UserStore=useUserStore()
+    UserStore.lastActive=moment().format('YYYY-MM-DD HH:mm:ss')
+}
+
 </script>
 
 <style scoped>
