707ca58521f9631328f37865b15acda5918b9734
Alva2452 <13312262862@163.com>
2025-11-20 02:01:54 +0800

    fix(getLoanProduct):添加分页查询

--- composables/getLoanProduct.ts
+++ composables/getLoanProduct.ts
@@ -1,18 +1,23 @@
-import { useAxios } from "./useAxios"
-import type{ AgriculturalLoanProductList } from "~/types/loanProduct";
-const useAxiosInstance=useAxios()
-export async function getLoanProductList(){
-    try{
-        const response=await useAxiosInstance.get<{data:AgriculturalLoanProductList}>('/loan/list')
-        if(response.status===200){
-            console.log('获取产品列表成功',response.data)
-            return response.data.data
-        }
-        else{
-            throw new Error('获取产品列表失败')
-        }
+import { useAxios } from "./useAxios";
+import type { AgriculturalLoanProductList } from "~/types/loanProduct";
+const useAxiosInstance = useAxios();
+export async function getLoanProductList(page: number, pageSize: number) {
+  try {
+    const response = await useAxiosInstance.get<{
+      data: AgriculturalLoanProductList;
+    }>("/loan/list", {
+      params: {
+        page,
+        pageSize,
+      },
+    });
+    if (response.status === 200) {
+      console.log("获取产品列表成功", response.data);
+      return response.data.data;
+    } else {
+      throw new Error("获取产品列表失败");
     }
-    catch(err){
-        console.error('获取产品列表失败',err)
-    }
+  } catch (err) {
+    console.error("获取产品列表失败", err);
+  }
 }
