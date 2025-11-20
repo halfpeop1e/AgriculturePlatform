b9d05d0a1b5f0ea69e1461c33ec483fe53826932
Alva2452 <13312262862@163.com>
2025-11-20 15:08:02 +0800

    feat:添加查看贷款进度功能

--- composables/getLoanProduct.ts
+++ composables/getLoanProduct.ts
@@ -1,5 +1,6 @@
+import type { ApplyListRespond } from "~/types/loanApply";
 import { useAxios } from "./useAxios";
-import type { AgriculturalLoanProductList } from "~/types/loanProduct";
+import type { AgriculturalLoanProduct, AgriculturalLoanProductList } from "~/types/loanProduct";
 const useAxiosInstance = useAxios();
 export async function getLoanProductList(page: number, pageSize: number) {
   try {
@@ -9,6 +10,26 @@ export async function getLoanProductList(page: number, pageSize: number) {
       params: {
         page,
         pageSize,
+      },
+    });
+    if (response.status === 200) {
+      console.log("获取产品列表成功", response.data);
+      return response.data.data;
+    } else {
+      throw new Error("获取产品列表失败");
+    }
+  } catch (err) {
+    console.error("获取产品列表失败", err);
+  }
+}
+
+export async function getLoanApplyList(userId: string) {
+  try {
+    const response = await useAxiosInstance.get<{
+      data: ApplyListRespond[];
+    }>("/loan/list", {
+      params: {
+        user_id : userId,
       },
     });
     if (response.status === 200) {
---
+++ types/loanApply.d.ts
@@ -0,0 +1,9 @@
+export interface ApplyListRespond {
+  id: number;
+  uuid: string;
+  user_id: string;
+  product_id: string;
+  avatar: string;
+  product_name: string;
+  status: string;
+}
