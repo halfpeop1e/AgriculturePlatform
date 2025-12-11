32f97a4f14f327e746acdd4cd5345963b80199a8
Alva2452 <13312262862@163.com>
2025-12-12 00:04:52 +0800

    feat(editerProductRequest):添加编辑订单请求

---
+++ webServer/internal/dto/request/editer_product_request.go
@@ -0,0 +1,16 @@
+package request
+
+type EditerProductRequestWrapper struct {
+	FormData EditerProductRequest `json:"formData"`
+}
+type EditerProductRequest struct {
+	Name         string   `form:"name" json:"name"`
+	Images       []string `form:"images" json:"images"`
+	Category     string   `form:"category" json:"category"`
+	Description  string   `form:"description" json:"description"`
+	Price        float64  `form:"price" json:"price"`
+	Stock        int64    `form:"stock" json:"stock"`
+	Saler        string   `form:"saler" json:"saler"`
+	SalerId      string   `form:"salerId" json:"salerId"`
+	ContactEmail string   `form:"contactEmail" json:"contactEmail"`
+}
