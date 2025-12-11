34f27f6c5e677677d16bf3819769de683932594b
Alva2452 <13312262862@163.com>
2025-12-12 00:48:10 +0800

    feat(loanProcess):添加贷款进度数据库

---
+++ webServer/internal/model/loan_process.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type LoanProcess struct {
+	Id        int64          `gorm:"column:id;primarykey;comment:自增id"`
+	Uuid      string         `gorm:"column:uuid;uniqueIndex;type:varchar(20);not null;comment:贷款uuid"`
+	UserId    string         `gorm:"column:user_id;index;type:varchar(20);not null;comment:用户名ID"`
+	ProductID string         `gorm:"column:product_id;type:varchar(100);not null;comment:产品ID"`
+	Amount    float64        `gorm:"column:amount;type:decimal(10,2);not null;comment:贷款金额"`
+	Deadline  int64          `gorm:"column:deadline;type:int;not null;comment:贷款期限"`
+	Status    int16          `gorm:"column:phone;type:int;not null;comment:贷款状态 0-申请中 1-已通过 2-已拒绝 3-已还款"`
+	CreatAt   time.Time      `gorm:"column:create_at;type:timestamp;not null;comment:创建时间"`
+	DeleteAt  gorm.DeletedAt `gorm:"column:delete_at;type:timestamp;comment:删除时间"`
+}
