1666218b7b057e1811e604bb59ef14ed41c4f449
halfpeople <halfpeop1e@outlook.com>
2025-12-16 02:26:53 +0800

    feat: 添加返回首页面的导航项

--- config/settingRoute.ts
+++ config/settingRoute.ts
@@ -5,6 +5,8 @@ export const settingNav = [
       { label: '个人信息设置',routername:'/setting/profile',icon: 'pi pi-fw pi-shopping-cart' },
       { label: '安全性设置',routername:'/setting/security',icon: 'pi pi-fw pi-tags' },
       { label: '通知设置',routername:'/setting/notifications',icon: 'pi pi-fw pi-list' },
+      { label: '返回首页面',routername:'/middlepage',icon: 'pi pi-fw pi-angle-double-left' },
+     
     ],
   },
 ]
