797eac1c6e18ed0b4d565eb69ffe7ed05be6f707
halfpeop1e <halfpeop1e@outlook.com>
2025-12-18 18:54:18 +0800

    feat: 更新 Axios 实例的 baseURL 为生产环境地址

--- composables/useEmailAxios.ts
+++ composables/useEmailAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const useEmailAxios = () => {
   const AxiosInstance = axios.create({
-    baseURL: "http://localhost:3001", // 对应你 Node.js 后端接口
+    baseURL: "http://123.57.194.112", // 对应你 Node.js 后端接口
     timeout: 5000,
   });
 
